ec09386b30800e2975c2efdf38b678fc3319c3d9
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-01-05 14:58:15 -0800

    Documented faux_census dataset

---
+++ R/faux_census.r
@@ -0,0 +1,10 @@
+#' A small sample of a fabricated census-like dataset
+#'
+#' A dataset containing fake demographic data, used in the `fauxnaif` vignette.
+#'
+#' @format A tibble with 20 rows and 6 variables.
+#'
+#' @source Fabricated
+#' @examples
+#' faux_census
+"faux_census"

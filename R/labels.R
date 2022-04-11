ff4b78c96318788eda97a66bbad33057dff89444
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-04-10 17:35:57 -0700

    Use `expr_text()` instead of `as_label()`

--- R/labels.R
+++ R/labels.R
@@ -2,7 +2,7 @@ get_x_label <- function(call) {
   full_call <- rlang::call_match(rlang::frame_call(call), rlang::frame_fn(call))
 
   x <- full_call[["x"]]
-  x_label <- rlang::as_label(x)
+  x_label <- rlang::expr_name(x)
 
   # In case `x` was passed in from the `magrittr` pipe
   if (x_label == ".") {
@@ -17,6 +17,6 @@ get_arg_labels <- function(call) {
 
   # Exclude the first element (the function) and the second element (`x`)
   args <- full_call[-1:-2]
-  arg_labels <- vcapply(args, rlang::as_label)
+  arg_labels <- vcapply(args, rlang::expr_name)
   arg_labels
 }
